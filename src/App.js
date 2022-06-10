bec1591a31c0b4c68e1618a2b495314bba71ebcc
JQUAN CALEK <jquancollins96@gmail.com>
2022-06-10 18:00:18 -0400

    wip:reactform

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [state,setState] = useState({
+    skills:[{skill:"Javascript",level:"4"}],
+    skill:"",
+    level:"3"
+  });
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newSkill = {
+      skill: state.skill,
+      level:state.level
+    };
+  
+  setState({
+    skills: [...state.skills,newSkill],
+    skill:"",
+    level:"3"
+    });
+    console.log(...state.skills,newSkill);
+  }
+
+  
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]:e.target.value
+    });
+  }
+
+  return(
+    <section>
+      <h2>dev skills</h2>
+      <hr/>
+      {state.skills.map((s) => (
+        <article key={s.skill}>
+          <div>{s.skill}</div><div>{s.level}</div>
+        </article>
+      ))}
+      <hr />
+      <form onSubmit={handleSubmit}
+      >
+        <label>
+          <span>skills</span>
+          <input
+           name="skill" 
+          value={state.skill} 
+          onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span>level</span>
+          <select 
+          name="level" 
+          value={state.level}
+          onChange={handleChange}
+          >
+            
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+          </select>
+        </label>
+        <button>add skill</button>
+      </form>
+    </section>
+   )
+  }
 export default App;
